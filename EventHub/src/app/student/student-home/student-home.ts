e1b76d13265cb8bd6174c2059832eea7c3d62157
Diya Patel <diyapatel2936@gmail.com>
2026-01-02 15:41:45 +0530

    assets ni magajmari

--- EventHub/src/app/student/student-home/student-home.ts
+++ EventHub/src/app/student/student-home/student-home.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-student-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './student-home.html',
-  styleUrl: './student-home.css',
+  styleUrls: ['./student-home.css']
 })
 export class StudentHome {
 
+  showMenu = false;
+
+  categories = [
+    { name: 'Workshop', image: '/career_guidance.png' },
+    { name: 'Seminar', image: '/ai_seminar.png' },
+    { name: 'Cultural', image: '/cultural_fest.png' },
+    { name: 'Sports', image: '/badminton_championship.png' },
+    { name: 'Social', image: '/blood_donation.png' },
+    { name: 'Exhibition', image: '/exhibition.jpg' }
+  ];
+
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  closeMenu() {
+    this.showMenu = false;
+  }
+
+  goToCategory(category: string) {
+    this.router.navigate(['/student/category-events'], {
+      queryParams: { category }
+    });
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
